ce6b487e840ae094fb120ee8168eca549ac70aea
duane rush <desertduane@yahoo.com>
2018-02-10 16:03:38 -0800

    create new posts

--- CasualClans/Controllers/PostController.cs
+++ CasualClans/Controllers/PostController.cs
@@ -6,6 +6,7 @@ using CasualClans.Data;
 using CasualClans.Data.Models;
 using CasualClans.Models.Forum.Reply;
 using CasualClans.Models.Post;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CasualClans.Controllers
@@ -13,10 +14,15 @@ namespace CasualClans.Controllers
     public class PostController : Controller
     {
         private readonly IPost _postService;
+        private readonly IForum _forumService;
 
-        public PostController(IPost postService)
+        private static UserManager<ApplicationUser> _userManager;
+
+        public PostController(IPost postService, IForum forumService, UserManager<ApplicationUser> userManager)
         {
             _postService = postService;
+            _forumService = forumService;
+            _userManager = userManager;
         }
 
         public IActionResult Index(int Id)
@@ -38,6 +44,46 @@ namespace CasualClans.Controllers
                 Replies = replies
             };
             return View(model);
+        }
+
+        public IActionResult Create(int Id)
+        {
+            //Id is Forum.Id
+            var forum = _forumService.GetById(Id);
+            var model = new NewPostModel
+            {
+                ForumName = forum.Title,
+                ForumId = forum.Id,
+                ForumImageUrl = forum.ImageUrl,
+                AuthorName = User.Identity.Name
+
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPost(NewPostModel model)
+        {
+            var userId = _userManager.GetUserId(User);
+            var user = await _userManager.FindByIdAsync(userId);
+            var post = BuildPost(model, user);
+
+            await _postService.Add(post);
+
+            //TODO Impliment user rating managment here
+
+            return RedirectToAction("Index", "Post", post.Id);
+        }
+
+        private Post BuildPost(NewPostModel model, ApplicationUser user)
+        {
+            return new Post
+            {
+                Title = model.Title,
+                Content = model.Content,
+                Created = DateTime.Now,
+                User = user
+            };
         }
 
         private IEnumerable<PostReplyModel> BuildPostReplies(IEnumerable<PostReply> replies)
