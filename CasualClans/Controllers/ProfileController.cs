effd6f0a5c062d6b93f6b9b0b0140a5b3c988d54
duane rush <desertduane@yahoo.com>
2018-02-15 17:06:25 -0800

    Upload and ApplicationUser services

---
+++ CasualClans/Controllers/ProfileController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CasualClans.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasualClans.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IApplicationUser _userService;
+        private readonly IUpload _uploadService;
+
+        public ProfileController(UserManager<ApplicationUser> userManager, IApplicationUser userService, IUpload uploadService)
+        {
+            _userManager = userManager;
+            _userService = userService;
+            _uploadService = uploadService;
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
