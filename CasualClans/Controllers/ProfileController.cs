36b013fa8d449f57e2c0cf354f84f8d9e326b81a
duane rush <desertduane@yahoo.com>
2018-02-17 19:56:26 -0800

    building out blob storage for profile images

--- CasualClans/Controllers/ProfileController.cs
+++ CasualClans/Controllers/ProfileController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using CasualClans.Data;
 using CasualClans.Data.Models;
@@ -8,6 +9,7 @@ using CasualClans.Models.ApplicationUser;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace CasualClans.Controllers
 {
@@ -16,6 +18,7 @@ namespace CasualClans.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IApplicationUser _userService;
         private readonly IUpload _uploadService;
+        private readonly IConfiguration _configuration;
 
         public ProfileController(UserManager<ApplicationUser> userManager, IApplicationUser userService, IUpload uploadService)
         {
@@ -47,6 +50,16 @@ namespace CasualClans.Controllers
         public async Task<IActionResult> UploadProfileImage(IFormFile file)
         {
             var userId = _userManager.GetUserId(User);
+
+            var connectionString = _configuration.GetConnectionString("AzureStorageAccount");
+
+            var container = _uploadService.GetBlobContainer(connectionString);
+
+            var contentDisposition = ContentDispositionHeaderValue.Parse(file.ContentDisposition);
+
+            var filename = contentDisposition.FileName.Trim('"');
+
+            var blockBlob = container.GetBlockBlobReference(filename);
 
             //connect to azure storage container
             //Get blob container
